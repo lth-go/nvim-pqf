19508b25b295a1d467fffcfc1a064ed1f0bec815
李天华 <litianhua@moresec.cn>
2024-12-16 15:23:05 +0800

    optimize

--- lua/pqf/init.lua
+++ lua/pqf/init.lua
@@ -1,5 +1,6 @@
 local api = vim.api
 local fn = vim.fn
+
 local M = {}
 
 local signs = {
@@ -9,23 +10,43 @@ local signs = {
   hint = { text = 'H', hl = 'DiagnosticSignHint' },
 }
 
+local type_mapping = {
+  E = signs.error,
+  W = signs.warning,
+  I = signs.info,
+  N = signs.hint,
+}
+
 local namespace = api.nvim_create_namespace('pqf')
-local show_multiple_lines = false
 local max_filename_length = 0
 local filename_truncate_prefix = '[...]'
 
-local function pad_right(string, pad_to)
-  local new = string
-
+local function pad_left(s, pad_to)
   if pad_to == 0 then
-    return string
+    return s
   end
 
-  for i = fn.strwidth(string), pad_to do
-    new = new .. ' '
+  local width = fn.strwidth(s)
+
+  if width >= pad_to then
+    return s
   end
 
-  return new
+  return string.rep(' ', pad_to - width) .. s
+end
+
+local function pad_right(s, pad_to)
+  if pad_to == 0 then
+    return s
+  end
+
+  local width = fn.strwidth(s)
+
+  if width >= pad_to then
+    return s
+  end
+
+  return s .. string.rep(' ', pad_to - width)
 end
 
 local function trim_path(path)
@@ -43,9 +64,9 @@ end
 local function list_items(info)
   if info.quickfix == 1 then
     return fn.getqflist({ id = info.id, items = 1, qfbufnr = 1 })
-  else
-    return fn.getloclist(info.winid, { id = info.id, items = 1, qfbufnr = 1 })
   end
+
+  return fn.getloclist(info.winid, { id = info.id, items = 1, qfbufnr = 1 })
 end
 
 local function apply_highlights(bufnr, highlights)
@@ -64,17 +85,11 @@ function M.format(info)
   local list = list_items(info)
   local qf_bufnr = list.qfbufnr
   local raw_items = list.items
-  local lines = {}
-  local pad_to = 0
-  local type_mapping = {
-    E = signs.error,
-    W = signs.warning,
-    I = signs.info,
-    N = signs.hint,
-  }
-
   local items = {}
+
   local show_sign = false
+  local pad_to = 0
+  local num_pad_to = 0
 
   -- If we're adding a new list rather than appending to an existing one, we
   -- need to clear existing highlights.
@@ -87,131 +102,157 @@ function M.format(info)
 
     if raw then
       local item = {
-        type = raw.type,
-        text = raw.text,
-        location = '',
-        path_size = 0,
-        line_col_size = 0,
         index = i,
+        sign = ' ',
+        sign_hl = nil,
+        location = '',
+        location_size = 0,
+        lnum = '',
+        lnum_size = 0,
+        text = raw.text,
       }
 
-      if type_mapping[item.type] then
+      --
+      --
+      --
+
+      local sign_conf = type_mapping[raw.type]
+      if sign_conf then
+        item.sign = sign_conf.text
+        item.sign_hl = sign_conf.hl
+
         show_sign = true
       end
 
+      --
+      --
+      --
+
       if raw.bufnr > 0 then
         item.location = trim_path(fn.bufname(raw.bufnr))
-        item.path_size = #item.location
+        item.location_size = fn.strwidth(item.location)
+
+        if item.location_size > pad_to then
+          pad_to = item.location_size
+        end
       end
 
-      if raw.lnum and raw.lnum > 0 then
-        local lnum = raw.lnum
+      --
+      --
+      --
 
-        if raw.end_lnum and raw.end_lnum > 0 and raw.end_lnum ~= lnum then
+      if raw.lnum and raw.lnum > 0 then
+        local lnum = tostring(raw.lnum)
+
+        if raw.end_lnum and raw.end_lnum > 0 and raw.end_lnum ~= raw.lnum then
           lnum = lnum .. '-' .. raw.end_lnum
         end
 
-        if #item.location > 0 then
-          item.location = item.location .. ' ' .. lnum
-        else
-          item.location = tostring(lnum)
+        item.lnum = lnum
+        item.lnum_size = fn.strwidth(item.lnum)
+
+        if item.lnum_size > num_pad_to then
+          num_pad_to = item.lnum_size
         end
-
-        -- Column numbers without line numbers make no sense, and may confuse
-        -- the user into thinking they are actually line numbers.
-        if raw.col and raw.col > 0 then
-          local col = raw.col
-
-          if raw.end_col and raw.end_col > 0 and raw.end_col ~= col then
-            col = col .. '-' .. raw.end_col
-          end
-
-          item.location = item.location .. ':' .. col
-        end
-
-        item.line_col_size = #item.location - item.path_size
       end
 
-      local size = fn.strwidth(item.location)
+      --
+      --
+      --
 
-      if size > pad_to then
-        pad_to = size
-      end
+      -- Quickfix items only support singe-line messages, and show newlines as
+      -- funny characters. In addition, many language servers (e.g.
+      -- rust-analyzer) produce super noisy multi-line messages where only the
+      -- first line is relevant.
+      --
+      -- To handle this, we only include the first line of the message in the
+      -- quickfix line.
+      local text = vim.split(raw.text, '\n')[1]
+      item.text = fn.trim(text)
 
       table.insert(items, item)
     end
   end
 
+  local lines = {}
   local highlights = {}
 
   for _, item in ipairs(items) do
+    local line = ''
     local line_idx = item.index - 1
 
-    -- Quickfix items only support singe-line messages, and show newlines as
-    -- funny characters. In addition, many language servers (e.g.
-    -- rust-analyzer) produce super noisy multi-line messages where only the
-    -- first line is relevant.
     --
-    -- To handle this, we only include the first line of the message in the
-    -- quickfix line.
-    local text = vim.split(item.text, '\n')[1]
-    local location = item.location
+    --
+    --
 
-    -- Optionally show multiple lines joined with single space
-    if show_multiple_lines then
-      text = fn.substitute(item.text, '\n\\s*', ' ', 'g')
+    if show_sign and item.sign_hl then
+      line = line .. item.sign
+
+      table.insert(highlights, {
+        group = item.sign_hl,
+        line = line_idx,
+        col = 0,
+        end_col = #item.sign,
+      })
     end
 
-    text = fn.trim(text)
+    --
+    --
+    --
 
-    if text ~= '' then
-      location = pad_right(location, pad_to)
+    if item.location_size > 0 then
+      if line ~= '' then
+        line = line .. ' '
+      end
+
+      local col = fn.strwidth(line)
+      line = line .. pad_right(item.location, pad_to)
+
+      table.insert(highlights, {
+        group = 'Directory',
+        line = line_idx,
+        col = col,
+        end_col = col + item.location_size,
+      })
     end
 
-    local sign_conf = type_mapping[item.type]
-    local sign = ' '
-    local sign_hl = nil
+    --
+    --
+    --
 
-    if sign_conf then
-      sign = sign_conf.text
-      sign_hl = sign_conf.hl
+    if item.lnum_size > 0 then
+      if line ~= '' then
+        line = line .. ' '
+      end
+
+      local col = fn.strwidth(line)
+      line = line .. '|' .. pad_left(item.lnum, num_pad_to) .. '|'
+
+      table.insert(highlights, {
+        group = 'Number',
+        line = line_idx,
+        col = col,
+        end_col = col + num_pad_to + 2,
+      })
     end
 
-    local prefix = show_sign and sign .. ' ' or ''
-    local line = prefix .. location .. text
+    --
+    --
+    --
+
+    if item.text ~= '' then
+      if line ~= '' then
+        line = line .. ' '
+      end
+
+      line = line .. item.text
+    end
 
     -- If a line is completely empty, Vim uses the default format, which
     -- involves inserting `|| `. To prevent this from happening we'll just
     -- insert an empty space instead.
     if line == '' then
       line = ' '
-    end
-
-    if show_sign and sign_hl then
-      table.insert(
-        highlights,
-        { group = sign_hl, line = line_idx, col = 0, end_col = #sign }
-      )
-    end
-
-    if item.path_size > 0 then
-      table.insert(highlights, {
-        group = 'Directory',
-        line = line_idx,
-        col = #prefix,
-        end_col = #prefix + item.path_size,
-      })
-    end
-
-    if item.line_col_size > 0 then
-      local col_start = #prefix + item.path_size
-
-      table.insert(highlights, {
-        group = 'Number',
-        line = line_idx,
-        col = col_start,
-        end_col = col_start + item.line_col_size,
-      })
     end
 
     table.insert(lines, line)
@@ -232,10 +273,6 @@ function M.setup(opts)
   if opts.signs then
     assert(type(opts.signs) == 'table', 'the "signs" option must be a table')
     signs = vim.tbl_deep_extend('force', signs, opts.signs)
-  end
-
-  if opts.show_multiple_lines then
-    show_multiple_lines = true
   end
 
   if opts.max_filename_length then
